6ceefae15e0a65a74c669fb961565736849ddbf8
abarecakes07 <abaremj078@gmail.com>
2026-08-04 02:44:11 +0800

    fix

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
      sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom',],
-        },
+       
       },
     },
   },
